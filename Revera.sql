9ada4ecbfd5d64e68552e906621ab7979558714e
trpdat <trpdat@gmail.com>
2020-08-20 22:58:12 +0700

    V4

--- Revera.sql
+++ Revera.sql
@@ -1,34 +1,106 @@
-SELECT
-E.FLIGHT_NO,
-E.UPLIFT_DATE,
-E.ISS_AIRLINE,
-E.DOC_NO,
-E.COUPON_NO,
-E.DOC_CLASS,
-E.FROM_CITY || E.TO_CITY AS SECTOR,
-E.CLASS,
-E.AOS,
-SUM(DECODE(E.ISS_AIRLINE, 738, 
-                    REVERA_OWNER.INTERNAL_CURR_CONV(E.AUDIT_PRORATE_CURRENCY,
+SELECT ALL 
+DECODE(F.ISS_AIRLINE, 738, '738', 'OA') "ISS_AIRLINE",
+F.FLIGHT_NO,
+F.DOC_NO,
+F.COUPON_NO,
+TO_CHAR(F.UPLIFT_DATE, 'YYYYMMDD') FLT_DATE,
+TO_CHAR(F.UPLIFT_DATE, 'MM') FLT_MONTH,
+TO_CHAR(F.UPLIFT_DATE, 'yyyy') FLT_YEAR,
+F.AOS,
+M.SALES_SOURCE,
+F.FROM_CITY||F.TO_CITY Sector,
+DECODE(UPC.COUNTRY_CODE, TOC.COUNTRY_CODE, 'D', 'I') Rtyp,
+SUM(DECODE(F.PAX_TYPE, 'A', 1, 0)*NVL(F.PAX_COUNT, 0)) adl,
+SUM(DECODE(F.PAX_TYPE, 'C', 1, 0)*NVL(F.PAX_COUNT, 0)) chd,
+SUM(DECODE(F.PAX_TYPE, 'I', 1, 0)*NVL(F.PAX_COUNT, 0)) Inf,
+SUM(NVL(F.PAX_COUNT, 0)) Pax_cnt,
+SUM(DECODE(F.NON_REV_FLAG, 'Y', 1, 0)*NVL(F.PAX_COUNT, 0)) Foc,
+COUNT(F.DOC_NO) CPNS_COUNT,
+F.DOC_CLASS, DECODE(F.CLASS, NULL,'  ', 'F', 'F', 'C', 'C', 'D', 'C', 'J', 'C', 'I', 'I', 'Y') SERVICE_CLASS,
+F.PAX_TYPE, DECODE(F.ISS_AIRLINE, 738, F.AUDIT_PRORATE_CURRENCY, F.PRORATE_CURRENCY) curr,
+SUM(DECODE(F.ISS_AIRLINE, 738, F.AUDIT_SECTOR_AMOUNT_SC, F.SECTOR_VALUE)) "REVENUE-NTE",
+SUM(T.YQ_NTE),
+SUM(T.YR_NTE),
+SUM(DECODE(F.ISS_AIRLINE, 738, 
+                    REVERA_OWNER.INTERNAL_CURR_CONV(F.AUDIT_PRORATE_CURRENCY,
                                                                                  'VND', 
-                                                                                 E.DATE_OF_ISSUE, 
-                                                                                 E.AUDIT_SECTOR_AMOUNT_SC),
-                     REVERA_OWNER.INTERNAL_CURR_CONV(E.PRORATE_CURRENCY, 
+                                                                                 F.DATE_OF_ISSUE, 
+                                                                                 F.AUDIT_SECTOR_AMOUNT_SC),
+                     REVERA_OWNER.INTERNAL_CURR_CONV(F.PRORATE_CURRENCY, 
                                                                                 'VND', 
-                                                                                E.UPLIFT_DATE, 
-                                                                                E.SECTOR_VALUE))) "REVENUE-VND"
+                                                                                F.UPLIFT_DATE, 
+                                                                                F.SECTOR_VALUE))) "REVENUE-VND",
+SUM(T.YQ_VND),
+SUM(T.YR_VND),
+UPC.COUNTRY_CODE ORI_COUNTRY,
+TOC.COUNTRY_CODE DES_COUNTRY,
+CASE
+    WHEN F.FLIGHT_NO <8000 THEN 'VN'
+    WHEN F.FLIGHT_NO >8999 THEN 'VN'
+    ELSE '0V'
+END AS CARRIER
 FROM
-EFF_REP_TABLE E
-where
-uplift_date between '01-jan-2020' and '31-mar-2020'
-and doc_class in ('PAX', 'FIM')
+CITY_MASTER UPC,
+CITY_MASTER TOC,
+REVERA_OWNER.EFF_REP_TABLE F
+LEFT JOIN
+(
+    SELECT
+    ISS_AIRLINE,
+    DOC_NO,
+    COUPON_NO,
+    SUM(DECODE(TAX_CODE,'YQ',AUDIT_TAX_AMOUNT,NULL)) AS YQ_NTE,
+    SUM(DECODE(TAX_CODE,'YR',AUDIT_TAX_AMOUNT,NULL)) AS YR_NTE,
+    SUM(DECODE(TAX_CODE, 'YQ', AUDIT_TAX_AMOUNT_LC)) AS YQ_VND,
+    SUM(DECODE(TAX_CODE, 'YR', AUDIT_TAX_AMOUNT_LC)) AS YR_VND
+    FROM
+    TICKET_TAX_DETAIL
+    WHERE
+    TAX_CODE IN ('YQ','YR')
+    AND UTIL_TYPE = 'U'
+    AND MATCHED_FLAG = 'Y'
+    GROUP BY
+    ISS_AIRLINE,
+    DOC_NO,
+    COUPON_NO
+) T
+ON
+(
+F.ISS_AIRLINE = T.ISS_AIRLINE
+AND F.DOC_NO = T.DOC_NO
+AND F.COUPON_NO = T.COUPON_NO
+)
+LEFT JOIN TICKET_MAIN M
+ON ( F.ISS_AIRLINE = M.ISS_AIRLINE
+AND F.DOC_NO = M.DOC_NO
+)
+WHERE
+F.UPLIFT_DATE BETWEEN '01-JAN-2019' AND '01-JAN-2019'
+AND  ((F.UPLIFT_STATION=UPC.CITY_CODE_ALPHA)
+AND (F.TO_CITY=TOC.CITY_CODE_ALPHA))
+AND F.FLIGHT_NO <9000
+--AND ((M.PRODUCT_TYPE <> 'CH') OR (M.PRODUCT_TYPE IS NULL))
 GROUP BY
-E.FLIGHT_NO,
-E.UPLIFT_DATE,
-E.ISS_AIRLINE,
-E.DOC_NO,
-E.COUPON_NO,
-E.DOC_CLASS,
-E.FROM_CITY || E.TO_CITY,
-E.CLASS,
-E.AOS
+DECODE(F.ISS_AIRLINE, 738, '738', 'OA'),
+F.FLIGHT_NO,
+F.DOC_NO,
+F.COUPON_NO,
+TO_CHAR(F.UPLIFT_DATE, 'YYYYMMDD'),
+TO_CHAR(F.UPLIFT_DATE, 'MM'),
+TO_CHAR(F.UPLIFT_DATE, 'yyyy'),
+F.AOS,
+M.SALES_SOURCE,
+F.FROM_CITY||F.TO_CITY,
+DECODE(UPC.COUNTRY_CODE, TOC.COUNTRY_CODE, 'D', 'I'), 
+F.DOC_CLASS,
+DECODE(F.CLASS, NULL,'  ', 'F', 'F', 'C', 'C', 'D', 'C', 'J', 'C', 'I', 'I', 'Y'),
+F.PAX_TYPE, 
+DECODE(F.ISS_AIRLINE, 738, F.AUDIT_PRORATE_CURRENCY, F.PRORATE_CURRENCY),
+UPC.COUNTRY_CODE, 
+TOC.COUNTRY_CODE,
+--DECODE(F.DOC_CLASS,'MCO', DECODE(SUBSTR(F.DOC_NO, 1, 4), '9980', 'PXC', 'PXR'), 'PXR'),
+CASE
+    WHEN F.FLIGHT_NO <8000 THEN 'VN'
+    WHEN F.FLIGHT_NO >8999 THEN 'VN'
+    ELSE '0V'
+END
